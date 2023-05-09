927af6c7122b01bf4fe9808c025835273f6da0cb
rhoumi <remi.georges83@gmail.com>
2023-05-09 14:20:13 +0200

    correct fixed point arch

--- minimal-fixed-point_nomain.cpp
+++ minimal-fixed-point_nomain.cpp
@@ -47,8 +47,15 @@
 
 #include "ap_fixed.h"
 
-typedef ap_fixed<32, 8, AP_RND_CONV, AP_SAT> fixpoint_t;
 
+
+typedef ap_fixed<32,8,AP_RND_CONV,AP_SAT> fixpoint_t;
+// m: position of the most significant bit of the value, without taking the sign bit into account
+// l: LSB with negative coding
+#define sfx_t(m,l) ap_fixed<((m+1)-l+1),(m+1),AP_RND_CONV,AP_SAT>
+#define ufx_t(m,l) ap_ufixed<((m+1)-l),(m+1),AP_RND_CONV,AP_SAT>
+
+/*
 // fx version
 inline fixpoint_t fabsfx(fixpoint_t x)
 {
@@ -91,7 +98,7 @@ inline fixpoint_t exp10fx(fixpoint_t x)
 #ifdef __APPLE__
     return fixpoint_t(__exp10f(float(x)));
 #else
-    return fixpoint_t(exp10(float(x)));
+    return fixpoint_t(std::exp10(float(x)));
 #endif
 }
 inline fixpoint_t floorfx(fixpoint_t x)
@@ -151,6 +158,109 @@ inline fixpoint_t fmaxfx(fixpoint_t x, fixpoint_t y)
 {
     return fixpoint_t(std::max(float(x), float(y)));
 }
+*/
+
+// fx version
+inline float fabsfx(float x)
+{
+    return std::fabs(x);
+}
+inline float acosfx(float x)
+{
+    return std::acos(x);
+}
+inline float asinfx(float x)
+{
+    return std::asin(x);
+}
+inline float atanfx(float x)
+{
+    return std::atan(x);
+}
+inline float atan2fx(float x, float y)
+{
+    return std::atan2(x, y);
+}
+inline float ceilfx(float x)
+{
+    return std::ceil(x);
+}
+inline float cosfx(float x)
+{
+    return std::cos(x);
+}
+inline float expfx(float x)
+{
+    return std::exp(x);
+}
+inline float exp2fx(float x)
+{
+    return std::exp2(x);
+}
+inline float exp10fx(float x)
+{
+#ifdef __APPLE__
+    return __exp10f(x);
+#else
+    return exp10(x);
+#endif
+}
+inline float floorfx(float x)
+{
+    return std::floor(x);
+}
+inline float fmodfx(float x, float y)
+{
+    return std::fmod(x, y);
+}
+inline float logfx(float x)
+{
+    return std::log(x);
+}
+inline float log2fx(float x)
+{
+    return std::log2(x);
+}
+inline float log10fx(float x)
+{
+    return std::log10(x);
+}
+inline float powfx(float x, float y)
+{
+    return std::pow(x, y);
+}
+inline float remainderfx(float x, float y)
+{
+    return std::remainder(x, y);
+}
+inline float rintfx(float x)
+{
+    return std::rint(x);
+}
+inline float roundfx(float x)
+{
+    return std::round(x);
+}
+inline float sinfx(float x)
+{
+    return std::sin(x);
+}
+inline float sqrtfx(float x)
+{
+    return std::sqrt(x);
+}
+inline float tanfx(float x)
+{
+    return std::tan(x);
+}
+// min/max
+/*
+#define minfx(x, y) { (((x) < (y)) ? (x) : (y)) }
+#define maxfx(x, y) { (((x) < (y)) ? (y) : (x)) }
+*/
+
+inline float minfx(float x, float y) { return (x < y) ? x : y; }
+inline float maxfx(float x, float y) { return (x < y) ? y : x; }
 
 /******************************************************************************
  *******************************************************************************
